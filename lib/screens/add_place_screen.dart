74bc500ee80c8d28ab8d27655b49fc883bcef3be
Richie <esselrichmond78@gmail.com>
2022-03-07 23:17:21 +0000

    Initial Commit

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = "/add-place";
+
+  @override
+  State<AddPlaceScreen> createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add a new Place"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(
+                      labelText: "Title"
+                    ),
+                    controller: _titleController,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+
+                  ),
+                  FlatButton(onPressed: onPressed, child: child)
+                ],
+              ),
+            ),
+          )),
+          RaisedButton.icon(
+            onPressed: (){}, 
+            icon: Icon(Icons.add), 
+            label: Text("Add Place"), 
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap, 
+            color: Theme.of(context).accentColor,)
+        ],
+      ),
+    );
+  }
+}
