1d0adfe80a8c4bf0094206c96b060a86ba023c2e
Richie <esselrichmond78@gmail.com>
2022-03-10 03:19:14 +0000

    Added sql lite support to save data locally

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,11 +1,28 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import '../models/place.dart';
+import '../helpers/db_helpers.dart';
 
 class GreatPlaces with ChangeNotifier {
   List <Place> _items = [];
 
   List <Place> get items {
     return [..._items];
+  }
+
+  void addPlace(String Title, File image){
+    final newPlace = Place(id: DateTime.now().toString(), title: Title, location: null, image: image);
+    _items.add(newPlace);
+    print("object added");
+    notifyListeners();
+    DBHelper.insert('user_places', {'id': newPlace.id, 'title':newPlace.title, 'image': newPlace.image.path});
+  }
+
+  Future<void>fetchAndSetPlaces()async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList.map((item) =>Place(id: item['id'], title: item['title'], image:File(item['image']), location: null)).toList();
+    notifyListeners();
   }
 }
--- lib/widgets/image_inputs.dart
+++ lib/widgets/image_inputs.dart
@@ -1,9 +1,15 @@
  import 'dart:io';
  
  import 'package:flutter/material.dart';
+ import 'package:image_picker/image_picker.dart';
+ import 'package:path/path.dart' as path;
+ import 'package:path_provider/path_provider.dart' as syspaths;
 
  class ImageInput extends StatefulWidget {
    //const ImageInput({ Key? key }) : super(key: key);
+   final Function onSelectImage;
+
+   ImageInput(this.onSelectImage);
  
    @override
    State<ImageInput> createState() => _ImageInputState();
@@ -11,6 +17,22 @@
  
  class _ImageInputState extends State<ImageInput> {
    File _storedImage;
+
+   Future <void>_takePicture() async{
+     final picker = ImagePicker();
+    final  imageFile = await picker.getImage(source: ImageSource.camera, maxWidth: 600, );
+    if (imageFile == null){
+      return;
+    }
+    setState(() {
+      _storedImage = File(imageFile.path);
+    });
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName =path.basename(imageFile.path);
+    final savedImage = _storedImage.copySync('${appDir.path}/$fileName');
+    widget.onSelectImage(savedImage);
+
+   }
 
    @override
    Widget build(BuildContext context) {
@@ -32,7 +54,7 @@
            icon: Icon(Icons.camera),
            label: Text("Take Picture"),
            textColor: Theme.of(context).primaryColor,
-           onPressed: (){},
+           onPressed: _takePicture,
          ))
        ],
      );
